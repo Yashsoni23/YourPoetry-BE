da9df5c550c0e6a162d1de379b82286dc06fc8e6
Yash simejiya <yashsoni48678@gmail.com>
2023-03-24 23:22:25 +0530

    Deploy configration set

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors =  require("cors");
 const App = require("./src/app") 
-const port = 5000;
+// const port = 5000;
+const port = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use(cors({origin: "*"}));
